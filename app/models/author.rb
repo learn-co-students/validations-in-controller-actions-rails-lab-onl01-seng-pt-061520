f15012076fbb2327f299807c15074bfdd2678f99
mjaber <abujaber44@gmail.com>
2020-10-27 12:30:00 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates_uniqueness_of :email, on: :create, message: "must be unique"
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates_inclusion_of :category, in: %w( Fiction Non-Fiction ), on: :create, message: "extension %s is not included in the list"
+    validates :content, length: {minimum:100}
 end
